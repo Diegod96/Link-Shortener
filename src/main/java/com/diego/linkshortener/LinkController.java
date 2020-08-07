dabae6597d9461d4f9739cb0769695e6cb4480a9
Diegod96 <ddphillyfan@gmail.com>
2020-08-07 14:30:37 -0400

    LinkController Test Created

---
+++ src/main/java/com/diego/linkshortener/LinkController.java
@@ -0,0 +1,48 @@
+package com.diego.linkshortener;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class LinkController {
+
+    @PostMapping("/link")
+    Mono<CreateLinkResponse> create(@RequestBody CreateLinkRequest request) {
+        return Mono.just(new CreateLinkResponse("http://localhost:8080/aass2211"));
+    }
+
+    static class CreateLinkRequest {
+        private String link;
+
+        public String getLink() {
+            return link;
+        }
+
+        public void setLink(String link) {
+            this.link = link;
+        }
+    }
+
+    static class CreateLinkResponse {
+        CreateLinkResponse(String shortenedLink) {
+            this.shortenedLink = shortenedLink;
+        }
+
+        public String getShortenedLink() {
+            return shortenedLink;
+        }
+
+        public void setShortenedLink(String shortenedLink) {
+            this.shortenedLink = shortenedLink;
+        }
+
+        private String shortenedLink;
+
+
+    }
+
+
+}
